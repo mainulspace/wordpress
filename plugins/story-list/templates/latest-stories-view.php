97731e5f948e2f298cf2d6ddf69eaed7e210404c
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-06-26 13:03:53 +0200

    Improve code to ensure proper data escaping and enhance security
    - Added necessary escaping functions to display post titles and IDs securely
    - Escaped variables using esc_html() and esc_attr() functions

--- plugins/story-list/templates/latest-stories-view.php
+++ plugins/story-list/templates/latest-stories-view.php
@@ -1,67 +1,64 @@
 <div class="wrap top_stories_container">
 	<div class="story_list_cont">
 		<?php 
-			// get all top stories post 
-			$args = array( 		
-			'post_type'	=> 'post',
+		// Get latest stories
+		$args = array(
+			'post_type' => 'post',
 			'posts_per_page' => 20,
 			'meta_key' => 'latest_stories_sort_order',
-			'orderby'=>'meta_value',
-			'order' =>'ASC',
+			'orderby' => 'meta_value',
+			'order' => 'ASC',
 			'meta_query' => array(
 				array(
 					'key' => 'hrgmu_m_latest_story',
 					'value' => '1',
 					'compare' => '='
-					)
-				),		
-			);		
-			$posts_list = get_posts($args); 
+				)
+			)
+		);
+		$posts_list = get_posts($args);
 		?>
 		<h2>Latest Stories</h2>
 		<ul id="sorted_list" class="sort_list">
-		<?php 
-			if($posts_list): 
+			<?php if ($posts_list):
 				$i = 1;
-				foreach ($posts_list as $story): 					
-					?>
-					<li id="<?php echo $story->ID; ?>">
-						<?php echo $i.'. '.$story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y",strtotime($story->post_date)) . ' </span> '; ?>
+				foreach ($posts_list as $story): ?>
+					<li id="<?php echo esc_attr($story->ID); ?>">
+						<?php echo $i . '. ' . esc_html($story->post_title) . ' <span class="postedDate">' . date("g:i a j F Y", strtotime($story->post_date)) . '</span> '; ?>
 						<button class="st_delete">X</button>
-					</li>				
-			<?php
-				$i++;
-				endforeach; 
+					</li>
+					<?php $i++;
+				endforeach;
 			endif; ?>
 		</ul>
 	</div>
 	<div class="story_list_cont">
 		<?php 
-			// get all top stories post 
-			$args = array( 		
+		// Get recent stories
+		$args = array(
 			'post_type' => 'post',
-			'posts_per_page' => 200		
-			);		
-			$posts_list = get_posts($args); 
+			'posts_per_page' => 200
+		);
+		$posts_list = get_posts($args);
 		?>
 		<h2>Recent Stories</h2>
 		<ul id="un_sorted_list" class="sort_list">
-		<?php 
-			if($posts_list): 
-				foreach ($posts_list as $story): 
-					$feat_story = get_post_meta($story->ID, 'hrgmu_m_latest_story', true);				
-					if($feat_story)
+			<?php if ($posts_list):
+				foreach ($posts_list as $story):
+					$feat_story = get_post_meta($story->ID, 'hrgmu_m_latest_story', true);
+					if ($feat_story) {
 						continue;
+					}
 					?>
-					<li id="<?php echo $story->ID; ?>">						
-						<?php echo $story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y",strtotime($story->post_date)) . ' </span> '; ?>
+					<li id="<?php echo esc_attr($story->ID); ?>">
+						<?php echo esc_html($story->post_title) . ' <span class="postedDate">' . date("g:i a j F Y", strtotime($story->post_date)) . '</span> '; ?>
 						<button class="st_delete">X</button>
-					</li>				
-			<?php endforeach; 
+					</li>
+				<?php endforeach;
 			endif; ?>
 		</ul>
 	</div>
-	
+
 	<div class="clear"></div>
 
 	<p class="submit">
@@ -69,8 +66,7 @@
 	</p>
 	<script type="text/javascript">
 		var data = {
-			'action': 'latest-story-sort', //Set an action for our ajax function
+			'action': 'latest-story-sort', // Set an action for our AJAX function
 		};
 	</script>	
-	
 </div>
