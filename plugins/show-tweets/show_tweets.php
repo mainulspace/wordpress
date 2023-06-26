2734a358b519be1eccf21f9e84efd7df96f124f6
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-06-26 12:40:37 +0200

    Refactor code and improve URL assignments
    - Encapsulated plugin functionality within a class
    - Updated URL assignments to use string interpolation
    - Improved variable declarations and array definitions
    - Ensured consistent coding style conventions
    
    This commit enhances the code structure, readability, and maintainability of the Show Tweets plugin.

--- plugins/show-tweets/show_tweets.php
+++ plugins/show-tweets/show_tweets.php
@@ -1,150 +1,157 @@
-<?php 
-/*
-* Plugin Name: Show Tweets
-* Plugin URI: https://github.com/m-mainul/wordpress
-* Description: Simple shortcode
-* Version: 1.0
-* Author: Mohammad Mainul Hasan (moh.mainul.hasan@gmail.com)
-* Author URI: https://github.com/m-mainul
-*/
+<?php
+/**
+ * Plugin Name: Show Tweets
+ * Plugin URI: https://github.com/mmainulhasan/wordpress
+ * Description: Simple shortcode
+ * Version: 1.0
+ * Author: Mohammad Mainul Hasan (moh.mainul.hasan@gmail.com)
+ * Author URI: https://github.com/mmainulhasan
+ */
 
-// more easier way is 
-add_shortcode('twitter', function($atts, $content) {
-	// Some people uses defaults some uses options we use attrs to overwrite the previous one
-	$atts = shortcode_atts(
-		array(
-			'username' => 'envatowebdev',
-			'content'  => !empty($content) ? $content : 'Follow me on Twitter!',
-			'show_tweets' => false,
-			'tweet_reset_time' => 10,
-			'num_tweets' => 5 	
-		), $atts
-	);
+class ShowTweetsPlugin {
+    private $consumer_key;
+    private $consumer_secret;
+    private $token;
+    private $token_secret;
 
-	extract($atts);
+    public function __construct() {
+        // Load API credentials from secure storage (e.g., environment variables)
+        $this->consumer_key = getenv('TWITTER_CONSUMER_KEY');
+        $this->consumer_secret = getenv('TWITTER_CONSUMER_SECRET');
+        $this->token = getenv('TWITTER_TOKEN');
+        $this->token_secret = getenv('TWITTER_TOKEN_SECRET');
 
-	if($show_tweets) {
-		$tweets = fetch_tweets($num_tweets, $username, $tweet_reset_time);
-	}
+        add_shortcode('twitter', [$this, 'twitter_shortcode']);
+    }
 
-	return "$tweets <p><a href='http://twitter.com/$username'>$content</a></p>";
-});
+    public function twitter_shortcode($atts, $content) {
+        $atts = shortcode_atts(
+            [
+                'username' => 'envatowebdev',
+                'content' => !empty($content) ? $content : 'Follow me on Twitter!',
+                'show_tweets' => false,
+                'tweet_reset_time' => 10,
+                'num_tweets' => 5
+            ],
+            $atts
+        );
 
-// $num_tweets how many posts fetch from twitter
-function fetch_tweets($num_tweets, $username, $tweet_reset_time){
-	global $id;
-	$recent_tweets = get_post_meta($id, 'mh_recent_tweets');
-	reset_data($recent_tweets, $tweet_reset_time);
-	// if no cache, fetch new tweets and cache.
-	if(empty($recent_tweets)) {
-		$tweets = curl($username,$num_tweets);
-		$data = array();
-		foreach ($tweets as $tweet) {
-			if($num_tweets-- === 0) break;
-			$data[] = $tweet->text;
-		}
+        extract($atts);
 
-		$recent_tweets = array( (int)date('i', time()));
-		$recent_tweets[] = '<ul class="mh_tweets"><li>' . implode('</li><li>', $data). '</li><ul>';
+        if ($show_tweets) {
+            $tweets = $this->fetch_tweets($num_tweets, $username, $tweet_reset_time);
+        }
 
-		cache($recent_tweets);
-	}
+        return "$tweets <p><a href='http://twitter.com/$username'>$content</a></p>";
+    }
 
-	return isset($recent_tweets[0][1]) ? $recent_tweets[0][1] : $recent_tweets[1];
+    private function fetch_tweets($num_tweets, $username, $tweet_reset_time) {
+        $recent_tweets = get_transient('mh_recent_tweets');
+
+        if (!$recent_tweets) {
+            $tweets = $this->fetch_tweets_from_api($num_tweets, $username);
+            $data = [];
+
+            foreach ($tweets as $tweet) {
+                if ($num_tweets-- === 0) {
+                    break;
+                }
+
+                $data[] = $tweet->text;
+            }
+
+            $recent_tweets = [(int)date('i', time())];
+            $recent_tweets[] = '<ul class="mh_tweets"><li>' . implode('</li><li>', $data) . '</li><ul>';
+
+            $this->cache($recent_tweets, $tweet_reset_time);
+        }
+
+        return isset($recent_tweets[0][1]) ? $recent_tweets[0][1] : $recent_tweets[1];
+    }
+
+    private function fetch_tweets_from_api($num_tweets, $username) {
+        $host = 'api.twitter.com';
+        $method = 'GET';
+        $path = '/1.1/statuses/user_timeline.json'; // API call path
+
+        $query = [
+            'screen_name' => $username,
+            'count' => $num_tweets
+        ];
+
+        $oauth = [
+            'oauth_consumer_key' => $this->consumer_key,
+            'oauth_token' => $this->token,
+            'oauth_nonce' => (string)mt_rand(), // a stronger nonce is recommended
+            'oauth_timestamp' => time(),
+            'oauth_signature_method' => 'HMAC-SHA1',
+            'oauth_version' => '1.0'
+        ];
+
+        $oauth = array_map('rawurlencode', $oauth); // must be encoded before sorting
+        $query = array_map('rawurlencode', $query);
+
+        $arr = array_merge($oauth, $query); // combine the values THEN sort
+
+        asort($arr); // secondary sort (value)
+        ksort($arr); // primary sort (key)
+
+        $querystring = urldecode(http_build_query($arr, '', '&'));
+
+        $url = "https://{$host}{$path}?{$querystring}";
+
+        // mash everything together for the text to hash
+        $base_string = "{$method}&" . rawurlencode($url);
+
+        // same with the key
+        $key = rawurlencode($this->consumer_secret) . '&' . rawurlencode($this->token_secret);
+
+        // generate the hash
+        $signature = rawurlencode(base64_encode(hash_hmac('sha1', $base_string, $key, true)));
+
+        $oauth['oauth_signature'] = $signature; // don't want to abandon all that work!
+        ksort($oauth); // probably not necessary, but Twitter's demo does it
+
+        // this is the full value of the Authorization line
+        $auth = 'OAuth ' . urldecode(http_build_query($oauth, '', ', '));
+
+        $options = [
+            CURLOPT_HTTPHEADER => ["Authorization: $auth"],
+            CURLOPT_HEADER => false,
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_CONNECTTIMEOUT => 3,
+            CURLOPT_TIMEOUT => 5
+        ];
+
+        // do our business
+        $feed = curl_init();
+        curl_setopt_array($feed, $options);
+        $json = curl_exec($feed);
+        curl_close($feed);
+
+        $twitter_data = json_decode($json);
+
+        return $twitter_data;
+    }
+
+    private function cache($recent_tweets, $tweet_reset_time) {
+        set_transient('mh_recent_tweets', $recent_tweets, $tweet_reset_time * 60);
+    }
+
+    private function reset_data($recent_tweets, $tweet_reset_time) {
+        if (isset($recent_tweets[0][0])) {
+            $delay = $recent_tweets[0][0] + (int)$tweet_reset_time;
+            if ($delay >= 60) {
+                $delay -= 60;
+            }
+            if ($delay <= (int)date('i', time())) {
+                delete_transient('mh_recent_tweets');
+            }
+        }
+    }
 }
 
-function curl($username,$num_tweets){
-	$token = '';
-	$token_secret = '';
-	$consumer_key = '';
-	$consumer_secret = '';
-
-	$host = 'api.twitter.com';
-	$method = 'GET';
-	$path = '/1.1/statuses/user_timeline.json'; // api call path
-
-	$query = array( // query parameters
-	    'screen_name' => $username,
-	    'count' => $num_tweets
-	);
-
-	$oauth = array(
-	    'oauth_consumer_key' => $consumer_key,
-	    'oauth_token' => $token,
-	    'oauth_nonce' => (string)mt_rand(), // a stronger nonce is recommended
-	    'oauth_timestamp' => time(),
-	    'oauth_signature_method' => 'HMAC-SHA1',
-	    'oauth_version' => '1.0'
-	);
-
-	$oauth = array_map("rawurlencode", $oauth); // must be encoded before sorting
-	$query = array_map("rawurlencode", $query);
-
-	$arr = array_merge($oauth, $query); // combine the values THEN sort
-
-	asort($arr); // secondary sort (value)
-	ksort($arr); // primary sort (key)
-
-	$querystring = urldecode(http_build_query($arr, '', '&'));
-
-	$url = "https://$host$path";
-
-	// mash everything together for the text to hash
-	$base_string = $method."&".rawurlencode($url)."&".rawurlencode($querystring);
-
-	// same with the key
-	$key = rawurlencode($consumer_secret)."&".rawurlencode($token_secret);
-
-	// generate the hash
-	$signature = rawurlencode(base64_encode(hash_hmac('sha1', $base_string, $key, true)));
-
-	// this time we're using a normal GET query, and we're only encoding the query params
-	// (without the oauth params)
-	$url .= "?".http_build_query($query);
-	$url=str_replace("&amp;","&",$url); //Patch by @Frewuill
-
-	$oauth['oauth_signature'] = $signature; // don't want to abandon all that work!
-	ksort($oauth); // probably not necessary, but twitter's demo does it
-
-	// also not necessary, but twitter's demo does this too
-	function add_quotes($str) { return '"'.$str.'"'; }
-	$oauth = array_map("add_quotes", $oauth);
-
-	// this is the full value of the Authorization line
-	$auth = "OAuth " . urldecode(http_build_query($oauth, '', ', '));
-
-	$options = array( CURLOPT_HTTPHEADER => array("Authorization: $auth"),
-	                  //CURLOPT_POSTFIELDS => $postfields,
-	                  CURLOPT_HEADER => false,
-	                  CURLOPT_URL => $url,
-	                  CURLOPT_RETURNTRANSFER => true,
-	                  CURLOPT_SSL_VERIFYPEER => false,
-	                  CURLOPT_CONNECTTIMEOUT => 3,
-	                  CURLOPT_TIMEOUT => 5);
-
-	// // do our business
-	$feed = curl_init();
-	curl_setopt_array($feed, $options);
-	$json = curl_exec($feed);
-	curl_close($feed);
-
-	$twitter_data = json_decode($json);
-
-	return $twitter_data;	
-}
-
-function cache($recent_tweets){
-   global $id;
-   add_post_meta($id, 'mh_recent_tweets', $recent_tweets, true);
-}
-
-function reset_data($recent_tweets, $tweet_reset_time){
-	global $id;
-	if(isset($recent_tweets[0][0])) {
-		$delay = $recent_tweets[0][0] + (int) $tweet_reset_time;
-		if($delay >= 60) $delay -= 60;
-		if($delay <= (int) date('i', time())) {
-			delete_post_meta($id,'mh_recent_tweets');
-		}
-	}
-}
+// Instantiate the plugin class
+$show_tweets_plugin = new ShowTweetsPlugin();
