42e4a8b6012bbd7a5d1fdb2ef05f9d4eadd6c3ba
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-06-26 13:37:26 +0200

    Refactor code for improved structure and dynamic view loading

--- plugins/story-list/templates/story-list-view.php
+++ plugins/story-list/templates/story-list-view.php
@@ -1,42 +1,45 @@
-<?php 
-	// grab all the Request params 
-	$story_data = $_REQUEST;
+<?php
+// Grab all the request parameters
+$story_data = $_REQUEST;
 
-	// check any stories selectd or not (if not then select Fetured stories)
-	if(! isset($story_data['stories'])){
-		$story_data['stories'] = 1;
-	}
+// Check if any stories are selected (if not, default to Featured stories)
+$storyType = isset($story_data['stories']) ? $story_data['stories'] : 1;
 ?>
-<div class="updated ct_sort_updated" style="display:none;"><p><strong><?php _e('Sorting saved.', 'menu-test' ); ?></strong></p></div>
-<div class="error notice ct_sort_error" style="display:none;">
-    <p>There has been an error.</p>
-</div>
-<div id="sort_loading"><span class="sort_loading_animator"><div class='uil-ripple-css' style='transform:scale(1);'><div></div><div></div></div></span></div>
+
+<?php include('header.php'); ?>
+
 <div class="wrap">
 	<h1>Manage Contents</h1>
 	<ul class="subsubsub">
 		<li class="featured_stories">
-			<a <?php echo $story_data['stories'] == 1 ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=1">Featured / Top Stories</span></a> |
+			<a <?php echo ($storyType == 1) ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=1">Featured / Top Stories</a> |
 		</li>
 		<li class="worth_reading_stories">
-			<a <?php echo $story_data['stories'] == 3 ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=3">Worth Reading Stories</span></a> |
+			 <a <?php echo ($storyType == 3) ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=3">Worth Reading Stories</a> |
 		</li>
 		<li class="latest_stories">
-			<a <?php echo $story_data['stories'] == 4 ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=4">Latest Stories</span></a>
-		</li>		
-
+			<a <?php echo ($storyType == 4) ? 'class="current"' : ''; ?> href="edit.php?page=manage-story-list&stories=4">Latest Stories</a>
+		</li>
 	</ul>
 	<div class="clear"></div>
 </div>
 
-
-<?php 
-	if($story_data['stories'] == 1){
+<?php
+// Handle different story types
+switch ($storyType) {
+	case 1:
 		include('featured-stories-view.php');
-	}else if($story_data['stories'] == 3){
+		break;
+	case 3:
 		include('worth-reading-stories-view.php');
-	}else if($story_data['stories'] == 4){
+		break;
+	case 4:
 		include('latest-stories-view.php');
-	}
-	
+		break;
+	default:
+		echo 'Invalid story type.';
+		break;
+}
 ?>
+
+<?php include('footer.php'); ?>
