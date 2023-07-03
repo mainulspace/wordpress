78b9e73d9b8c3d8c9737daf3140be382b160a23d
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-07-03 19:01:51 +0200

    Optimize code for top stories and recent stories

--- plugins/story-list/templates/worth-reading-stories-view.php
+++ plugins/story-list/templates/worth-reading-stories-view.php
@@ -3,62 +3,61 @@
 		<?php 
 			// get all top stories post 
 			$args = array( 		
-			'post_type'	=> 'post',
-			'posts_per_page' => 20,
-			'meta_key' => 'worth_reading_stories_sort_order',
-			'orderby'=>'meta_value',
-			'order' =>'ASC',
-			'meta_query' => array(
-				array(
-					'key' => 'worth_reading',
-					'value' => '1',
-					'compare' => '='
+				'post_type'	=> 'post',
+				'posts_per_page' => 20,
+				'meta_key' => 'worth_reading_stories_sort_order',
+				'orderby' => 'meta_value',
+				'order' => 'ASC',
+				'meta_query' => array(
+					array(
+						'key' => 'worth_reading',
+						'value' => '1',
+						'compare' => '='
 					)
-				),		
+				)
 			);		
 			$posts_list = get_posts($args); 
 		?>
 		<h2>Worth Reading Stories</h2>
 		<ul id="sorted_list" class="sort_list">
-		<?php 
-			if($posts_list):
-				$i = 1; 
-				foreach ($posts_list as $story): 					
-					?>
-					<li id="<?php echo $story->ID; ?>">
-						<?php echo $i.'. '.$story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y",strtotime($story->post_date)) . ' </span> '; ?>
-						<button class="st_delete">X</button>
-					</li>				
-			<?php
-				$i++;
-				endforeach; 
-			endif; ?>
+		<?php if ($posts_list): ?>
+			<?php $i = 1; ?>
+			<?php foreach ($posts_list as $story): ?>
+				<li id="<?php echo $story->ID; ?>">
+					<?php echo $i . '. ' . $story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y", strtotime($story->post_date)) . ' </span> '; ?>
+					<button class="st_delete">X</button>
+				</li>
+				<?php $i++; ?>
+			<?php endforeach; ?>
+		<?php endif; ?>
 		</ul>
 	</div>
 	<div class="story_list_cont">
 		<?php 
-			// get all top stories post 
+			// get all recent stories post 
 			$args = array( 		
-			'post_type' => 'post',
-			'posts_per_page' => 300
+				'post_type' => 'post',
+				'posts_per_page' => 300,
+				'meta_query' => array(
+					array(
+						'key' => 'worth_reading',
+						'value' => '1',
+						'compare' => '!='
+					)
+				)
 			);		
 			$posts_list = get_posts($args); 
 		?>
 		<h2>Recent Stories</h2>
 		<ul id="un_sorted_list" class="sort_list">
-		<?php 
-			if($posts_list): 
-				foreach ($posts_list as $story): 
-					$feat_story = get_post_meta($story->ID, 'worth_reading', true);				
-					if($feat_story)
-						continue;
-					?>
-					<li id="<?php echo $story->ID; ?>">						
-						<?php echo $story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y",strtotime($story->post_date)) . ' </span> '; ?>
-						<button class="st_delete">X</button>
-					</li>				
-			<?php endforeach; 
-			endif; ?>
+		<?php if ($posts_list): ?>
+			<?php foreach ($posts_list as $story): ?>
+				<li id="<?php echo $story->ID; ?>">
+					<?php echo $story->post_title . ' <span class="postedDate"> ' . date("g:i a j F Y", strtotime($story->post_date)) . ' </span> '; ?>
+					<button class="st_delete">X</button>
+				</li>
+			<?php endforeach; ?>
+		<?php endif; ?>
 		</ul>
 	</div>
 	
@@ -69,8 +68,7 @@
 	</p>
 	<script type="text/javascript">
 		var data = {
-			'action': 'worth-reading-sort', //Set an action for our ajax function
+			'action': 'worth-reading-sort', // Set an action for our ajax function
 		};
 	</script>	
-	
 </div>
