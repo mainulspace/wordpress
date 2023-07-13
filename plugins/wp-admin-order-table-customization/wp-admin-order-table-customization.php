bfa57d7bfe21a828e7853908a0733dff123afd4f
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-07-13 19:03:16 +0200

    Optimize WP Admin Order Table Customization plugin code

--- plugins/wp-admin-order-table-customization/wp-admin-order-table-customization.php
+++ plugins/wp-admin-order-table-customization/wp-admin-order-table-customization.php
@@ -1,58 +1,57 @@
 <?php
 /**
  * Plugin Name: WP Admin Order Table Customization
- * Plugin URI: 
+ * Plugin URI:
  * Description: Add new columns in order table
  * Version: 1.0
  * Author: Mohammad Mainul Hasan (moh.mainul.hasan@gmail.com)
  * Author URI: https://github.com/mmainulhasan/wordpress
  */
 
-
 add_filter('manage_edit-shop_order_columns', 'wp_order_table_columns');
 function wp_order_table_columns($columns)
 {
-    $new_columns = array();
+    $updated_columns = array();
 
     foreach ($columns as $column_name => $column_info) {
-
-        $new_columns[$column_name] = $column_info;
-
-        echo $column_name;
-
-        if ('order_total' === $column_name) {
-            $new_columns['delivery_date'] = 'Delivery Date';
-        }
-        if ('order_status' === $column_name) {
-            $new_columns['vendor_order_status'] = 'Vendor Status';
-        }
-
+        $updated_columns[$column_name] = $column_info;
     }
 
-    return $new_columns;
+    $updated_columns['delivery_date'] = 'Delivery Date';
+    $updated_columns['vendor_order_status'] = 'Vendor Status';
+
+    return $updated_columns;
 }
 
 add_action('manage_shop_order_posts_custom_column', 'wp_order_table_columns_content', 10, 2);
 function wp_order_table_columns_content($column_name, $post_id)
 {
     if ('delivery_date' == $column_name) {
-        //Get delivery date from post meta
+        // Get delivery date from post meta
         $delivery_date = get_post_meta($post_id, 'vendor_delivery_date', true);
-        echo date('M d, Y', strtotime($delivery_date));
+        if (!empty($delivery_date)) {
+            echo date('M d, Y', strtotime($delivery_date));
+        }
+        return;
     }
+
     if ('vendor_order_status' == $column_name) {
-        //Get Vendor Order Status from post meta
-        echo get_post_meta($post_id, '_vendor_order_status', true);
+        // Get Vendor Order Status from post meta
+        $vendor_order_status = get_post_meta($post_id, '_vendor_order_status', true);
+        if (!empty($vendor_order_status)) {
+            echo $vendor_order_status;
+        }
+        return;
     }
 }
 
 add_filter('manage_edit-shop_order_sortable_columns', 'wp_order_table_sortable_columns');
-function wp_order_table_sortable_columns($columns)
+function wp_order_table_sortable_columns($sortable_columns)
 {
-    $columns['delivery_date'] = 'delivery_date';
-    $columns['vendor_order_status'] = 'vendor_order_status';
+    $sortable_columns['delivery_date'] = 'delivery_date';
+    $sortable_columns['vendor_order_status'] = 'vendor_order_status';
 
-    return $columns;
+    return $sortable_columns;
 }
 
 add_action('pre_get_posts', 'wp_order_table_columns_query_mods');
