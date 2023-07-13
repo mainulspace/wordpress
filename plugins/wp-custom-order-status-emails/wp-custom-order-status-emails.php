a2e494ef0a507570d13a52270153c647996fec23
Mainul Hasan <moh.mainul.hasan@gmail.com>
2023-07-13 19:28:34 +0200

    Optimize WP Site Custom Order Status Emails plugin code

--- plugins/wp-custom-order-status-emails/wp-custom-order-status-emails.php
+++ plugins/wp-custom-order-status-emails/wp-custom-order-status-emails.php
@@ -1,21 +1,28 @@
 <?php
 /**
- * Plugin Name: Wp Site Custom Order Status Emails
- * Plugin URI: 
- * Description: Will send email fors custom order statuses
+ * Plugin Name: WP Site Custom Order Status Emails
+ * Plugin URI: https://github.com/mmainulhasan/wordpress
+ * Description: Sends email for custom order statuses
  * Version: 0.1
  * Author: Mohammad Mainul Hasan (moh.mainul.hasan@gmail.com)
  * Author URI: https://github.com/mmainulhasan
  */
 
+use \WC_Email_Customer_Completed_Order;
 
 add_action('woocommerce_order_status_changed', 'wp_order_status_changed');
 
-function wp_order_status_changed($order_id) {
+function wp_order_status_changed(int $order_id): void
+{
     $order = wc_get_order($order_id);
     $order_status = $order->get_status();
-    if ($order_status == 'shipped') {
-        $wc_emails = wc()->mailer()->get_emails();
-        $wc_emails['WC_Email_Customer_Completed_Order']->trigger($order_id);
+
+    if ($order_status !== 'shipped') {
+        return; // No need to proceed further
     }
+
+    $wc_emails = wc()->mailer()->get_emails();
+    /** @var WC_Email_Customer_Completed_Order $customer_completed_order_email */
+    $customer_completed_order_email = $wc_emails['WC_Email_Customer_Completed_Order'];
+    $customer_completed_order_email->trigger($order_id);
 }
